ae3ca6adedc03df7d35caa4bb58c1110427bfa6c
Mike Childs <mike@mjcdev.co.uk>
2018-09-20 13:21:10 +0100

    #48245 - Skeleton Implementation of Data Retrieval

--- src/EPA/ESFA.DC.ReferenceData.EPA.Console/Program.cs
+++ src/EPA/ESFA.DC.ReferenceData.EPA.Console/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using ESFA.DC.ReferenceData.EPA.Model.EPA;
+using RestSharp;
 
 namespace ESFA.DC.ReferenceData.EPA.Console
 {
     class Program
     {
+        static IRestClient _client = new RestClient("https://findapprenticeshiptraining-api.sfa.bis.gov.uk/");
+
         static void Main(string[] args)
         {
+            ServicePoint servicePoint = ServicePointManager.FindServicePoint(new Uri("https://findapprenticeshiptraining-api.sfa.bis.gov.uk/"));
+            servicePoint.ConnectionLimit = 250;
+            servicePoint.UseNagleAlgorithm = false;
+            
+            var request = new RestRequest("assessment-organisations");
+
+            var organisations = _client.Execute<List<Organisation>>(request).Data;
+            
+            var organisationsPopulated = Task.WhenAll(organisations.Select(GetStandardsAndUpdateOrganisation)).Result;
+        }
+
+        public async static Task<Organisation> GetStandardsAndUpdateOrganisation(Organisation organisation)
+        {
+            var standardsRequest = new RestRequest("assessment-organisations/{organisationId}/standards");
+
+            standardsRequest.AddUrlSegment("organisationId", organisation.Id);
+
+            var standardsResponseTask = await _client.ExecuteTaskAsync<List<Standard>>(standardsRequest).ConfigureAwait(false);
+
+            organisation.Standards = standardsResponseTask.Data;
+
+            return organisation;
         }
     }
 }
--- src/EPA/ESFA.DC.ReferenceData.EPA.Model.EPA/Standard.cs
+++ src/EPA/ESFA.DC.ReferenceData.EPA.Model.EPA/Standard.cs
@@ -1,7 +1,11 @@
-﻿namespace ESFA.DC.ReferenceData.EPA.Model.EPA
+﻿using System.Collections.Generic;
+
+namespace ESFA.DC.ReferenceData.EPA.Model.EPA
 {
     public class Standard
     {
         public string StandardCode { get; set; }
+
+        public List<Period> Periods { get; set; }
     }
 }
