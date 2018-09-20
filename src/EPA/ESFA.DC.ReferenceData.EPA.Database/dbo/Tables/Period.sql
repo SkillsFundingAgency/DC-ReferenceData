432a8c0558aa3d5665e67882f56e75da0b78f585
Mike Childs <mike@mjcdev.co.uk>
2018-09-20 13:51:04 +0100

    #48245  - Database Project and SQL Tables Implemented

---
+++ src/EPA/ESFA.DC.ReferenceData.EPA.Database/dbo/Tables/Period.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Period]
+(
+	[OrganisationId] NVARCHAR(50) NOT NULL , 
+    [StandardCode] NVARCHAR(50) NOT NULL, 
+    [EffectiveFrom] DATETIME NOT NULL, 
+    [EffectiveTo] DATETIME NULL, 
+    PRIMARY KEY ([OrganisationId], [StandardCode], [EffectiveFrom]), 
+    CONSTRAINT [FK_Period_ToStandard] FOREIGN KEY ([OrganisationId], [StandardCode]) REFERENCES [Standard]([OrganisationId], [StandardCode])
+)
---
+++ src/EPA/ESFA.DC.ReferenceData.EPA.Database/dbo/Tables/Standard.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Standard]
+(
+	[OrganisationId] NVARCHAR(50) NOT NULL , 
+    [StandardCode] NVARCHAR(50) NOT NULL, 
+    PRIMARY KEY ([OrganisationId], [StandardCode]), 
+    CONSTRAINT [FK_Standard_ToOrganisation] FOREIGN KEY ([OrganisationId]) REFERENCES [Organisation]([Id])
+)
